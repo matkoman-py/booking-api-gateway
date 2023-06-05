a2f866b799c78653264c98d9d232bd7bc6303539
Mateja Cosovic <mateja@192.168.1.8>
2023-06-05 21:35:12 +0200

    feat: initial setup

---
+++ booking-api-gateway/src/main/java/com/bookingapigateway/BookingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bookingapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ booking-api-gateway/src/test/java/com/bookingapigateway/BookingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
